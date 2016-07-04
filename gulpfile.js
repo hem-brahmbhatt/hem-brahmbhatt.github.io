e956b54fff3ce31a52159584ebcbdb4de54affc7
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2016-07-04 13:24:20 +0100

    moved data out of templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
-var gulp = require('gulp'),
-    handlebars = require('gulp-compile-handlebars'),
-    rename = require('gulp-rename');
+const gulp = require('gulp'),
+      handlebars = require('gulp-compile-handlebars'),
+      rename = require('gulp-rename'),
+      yaml = require('js-yaml'),
+      fs   = require('fs');
 
-gulp.task('handlebars', function() {
+const templateData = Object.assign({}, ...[
+    './data/data.yaml',
+    './data/employment.yaml',
+    './data/open-source.yaml',
+    './data/personal-details.yaml',
+    './data/academics.yaml',
+    './data/summary.yaml'
+  ].map((file) => yaml.safeLoad(fs.readFileSync(file, 'utf8')))
+);
+
+
+gulp.task('handlebars', () => {
   gulp.src('templates/index.hbs')
-    .pipe(handlebars({}, {
-      batch: ['./templates']
+    .pipe(handlebars(templateData, {
+      helpers: require('./templates/helpers.js'),
+      batch: ['./templates'],
+      compile: { noEscape: true }
     }))
     .pipe(rename('index.html'))
     .pipe(gulp.dest('./'))
